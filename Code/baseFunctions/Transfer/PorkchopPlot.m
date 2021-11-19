d7772c959eb7d698a46fa0417aa932202c31eeb9
white <white@DESKTOP-4D8PE82.lan>
2021-11-19 15:07:38 +0100

    completed porkchop plot

--- Code/baseFunctions/Transfer/PorkchopPlot.m
+++ Code/baseFunctions/Transfer/PorkchopPlot.m
@@ -1,4 +1,4 @@
-function PorkchopPlot(departures, arrivals, Dvs)
+function PorkchopPlot(deps, arrs, Dvs)
 %PorkchopPlot ODE system for the two-body problem (Keplerian motion)
 %
 % PROTOTYPE:
@@ -18,12 +18,50 @@ function PorkchopPlot(departures, arrivals, Dvs)
 % VERSIONS
 % 2021-10-20: First version
 %   
-        
+    % mjd2000 To datenum
+    departures = zeros(size(deps, 2), 1); for x = 1:size(deps, 2); departures(x, :) = datenum(mjd20002date(deps(x))); end
+    arrivals = zeros(size(arrs, 2), 1); for x = 1:size(arrs, 2); arrivals(x, :) = datenum(mjd20002date(arrs(x))); end
+
+    % Plot range
+    min_Dv = ceil(min(min(Dvs)));
+    range = min_Dv:min_Dv+5;
+
+    % Find minimum value position inside the Dvs matrix
+    [x, y] = find(Dvs == min(min(Dvs)));
+    min_dep = departures(y);
+    min_arr = arrivals(x);
+
     % Meshgrid
     [deps, arrs] = meshgrid(departures, arrivals);
+    
+    % Creating a new figure
+    figure("name", "Porkchop Plot", "numbertitle", "off", "position", [100 100 900 700]);
 
-    figure("name", "Porkchop", "numbertitle", "off");
-    contour(deps, arrs, Dvs, min(min(Dvs)):min(min(Dvs)) + 5)
-    colorbar
+    % Contour plot
+    [C, h] = contour(deps, arrs, Dvs, range, 'LineWidth', 1.5);
+    hold on
+    grid on
+
+    % Title and labels
+    min_str = strcat(num2str(min(min(Dvs))), " km/s$)");
+    title(strcat("$\Delta v(t_1, t_2)$ ($\Delta v_{min} = ", min_str), "interpreter", "latex", "FontSize", 15);
+    xlabel("Departure date");
+    ylabel("Arrival date");
+
+    % Axis
+    xtickangle(45);
+
+    % Tick as date
+    datetick('x', 'yyyy mmm dd', 'keeplimits');
+    datetick('y', 'yyyy mmm dd', 'keeplimits');
+
+    % Plot minimum point
+    plot(min_dep, min_arr, '.', 'MarkerSize', 10);
+
+    % Addind a colorbar
+    caxis([min_Dv min_Dv+5])
+    clabel(C, h, range);
+    cb = colorbar;
+    title(cb, "$\Delta v\,[km/s]$", "interpreter", "latex");
 end
 
